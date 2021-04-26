35ed40cf54779e9d2e05772140ec3f0db51ee2f7
Lenovo <laramerdol@gmail.com>
2021-04-27 01:57:35 +0300

    secction
    secction

--- site/js/script.js
+++ site/js/script.js
@@ -136,39 +136,73 @@ var btn = document.getElementById("groupFormB");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+if(modal!== null){
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
     modal.style.display = "none";
   }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }  
 }
+
 
 //pop-up end
 
 
 
- let groups1 = ["group1", "group2"];
- let groups2 = ["group1", "group2"];
+    //write your code here
+    var group1={ name: "group1"};
+    var group2={ name: "group2"};
+    var group3={ name: "group3"};
+    var group4={ name: "group4"};
+   let groups1 = [group1, group2];
+   let groups2 = [group3, group4];
 
- var section1 = { group: groups1 , name: "Section1" };
- var section2 = { group: groups2 , name: "Section2" };
- //let sections = new Array();
-//sections.push(section1);
-//sections.push(section2);
-  var li = document.createElement("LI");  
-  li.value = section1.name;
-  var buttonE = document.createElement('button');
-  li.append(buttonE);
-  buttonE.onclick = function() { // remove list item here
+   var section1 = { group: groups1 , name: "Section1" };
+   var section2 = { group: groups2 , name: "Section2" };
+   var currentSection =section1 ;
+  let sections = new Array();
+  sections.push(section1);
+  sections.push(section2);
+  sections.forEach(function (element){
+      var li = document.createElement("LI");  
+      li.value = element;
+      var buttonE = document.createElement('button');
+      buttonE.innerText = element.name;
+      li.append(buttonE);
+      buttonE.onclick = function() { 
+        currentSection={ group: element.group, name: element.name};
+        if(document.getElementById("group-list").innerHTML!==""){
+          document.getElementById("group-list").innerHTML="";
 
-  };
-document.getElementById("section-list").appendChild(li);
+        }
+        
+        
+        renew();
+      };
+      document.getElementById("section-list").appendChild(li);  
+  });
+  function renew () {
+  currentSection.group.forEach(function(element){
+    var li = document.createElement("LI");  
+    li.value = element;
+    console.log(currentSection.group[1].name);
+    console.log(li);
+    var buttonE = document.createElement('button');
+    buttonE.innerText = element.name;
+    li.append(buttonE);
+     buttonE.onclick = function() { };  
+     document.getElementById("group-list").appendChild(li);  
+  });
+
+
+}
