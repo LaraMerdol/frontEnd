217ddc30d4b51668f2705197f4c5cf6fa3493f06
Lenovo <laramerdol@gmail.com>
2021-04-27 11:57:03 +0300

    peer-script added

---
+++ site/js/peer-script.js
@@ -0,0 +1,93 @@
+function clearContents(element) {
+  element.value = '';
+}
+
+var rateQuestionList = new Array();
+var MultipleChoiceQuestionList = new Array();
+var openEndedQuestionList = new Array();
+function addRateQuestion (){
+
+  const question = { questionText: document.getElementById("RateQuestionQ").value , questionRate :  document.getElementById("selectedRate").value }; 
+  rateQuestionList.push(question);
+  //console.log(rateQuestionList[size1-1].questionText );
+   var li = document.createElement("LI");
+   li.innerHTML =  question.questionText + "<br>"+ " Maximum Rate: "+ question.questionRate;
+   document.getElementById("Ratequestions").appendChild(li);
+
+
+} 
+
+function addOpenEndedQuestion (){
+  const question = { questionText: document.getElementById("OpenEndedQ").value}; 
+  openEndedQuestionList.push(question);
+  //console.log(rateQuestionList[0].questionText );
+   var li = document.createElement("LI");
+   li.innerHTML =  question.questionText ;
+   document.getElementById("OpenEndedquestions").appendChild(li);
+
+
+} 
+
+function addMultipleChoiceQuestion (){
+
+                     
+            
+  const lis = document.getElementById("answerList").innerText;
+  var coices = new Array;
+  var i = 0;
+  var j= 5;
+  var length =lis.length;
+  var k = 0;
+  while(j<= length){
+    if( lis.substring(i,j+1) == "delete"){
+      coices.push( lis.substring(k,i));
+      k=j+1;
+      i=i+6;
+      j=j+6;
+    }
+    i++;
+    j++;
+  }
+    const question = { questionText: document.getElementById("MultipleChoiceQ").value,
+                       coiceList:coices}; 
+
+
+    var li = document.createElement("LI");
+    li.innerHTML =  question.questionText + "<br>"+ question.coiceList.join("<br>");
+    document.getElementById("Choicequestions").appendChild(li);
+    MultipleChoiceQuestionList.push(question);
+  
+
+}
+
+function addItem(){
+  var li = document.createElement("LI");  
+  var input = document.getElementById("ChoiceText");
+  li.innerHTML = input.value;
+  
+  input.value = ChoiceText.innerHTML;
+  li.value =ChoiceText.innerHTML;
+  var buttonE = document.createElement('button');
+  li.append(buttonE);
+  buttonE.innerText = 'delete';
+  buttonE.onclick = function() { // remove list item here
+    li.remove();
+    buttonE.remove();
+  };
+document.getElementById("answerList").appendChild(li);
+
+        
+
+}
+
+   
+function submit () {
+     console.log("Submitted");
+     console.log("Open Ended Question List"+" ");
+     console.log(openEndedQuestionList);
+     console.log("Multiple Choice Question List"+" ");
+     console.log(MultipleChoiceQuestionList);
+     console.log("Rate Question List"+" ");
+     console.log(rateQuestionList);
+
+}   
